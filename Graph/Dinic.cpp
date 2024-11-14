91aae701fa619f1992adabe42772544c8402e34d
Khoi Le <letrongkhoi2008@gmail.com>
2024-11-14 22:41:21 +0700

    hallo

--- Graph/Dinic.cpp
+++ Graph/Dinic.cpp
@@ -2,17 +2,14 @@ const ll INF = 1e18;
 struct Dinic {
   const static bool SCALING = false;  // scaling = EV log(max C) with larger constant
   ll lim = 1;
-
   struct Edge {
     int u, v;
     ll cap, flow;
   };
-
   int n, s, t;
   vector<int> level, ptr;
   vector<Edge> e;
   vector<vector<int>> g;
-
   Dinic(int _n) : n(_n), level(_n), ptr(_n), g(_n) {
     e.clear();
     for (int i = 0; i < n; ++i) {
@@ -20,7 +17,6 @@ struct Dinic {
       g[i].clear();
     }
   }
-
   void add_edge(int u, int v, ll c) {
     debug(u, v, c);
     g[u].push_back(sz(e));
@@ -28,7 +24,6 @@ struct Dinic {
     g[v].push_back(sz(e));
     e.push_back({v, u, 0, 0});
   }
-
   ll get_max_flow(int _s, int _t) {
     s = _s, t = _t;
     ll flow = 0;
@@ -41,7 +36,6 @@ struct Dinic {
     }
     return flow;
   }
-
  private:
   bool bfs() {
     queue<int> q;
@@ -61,7 +55,6 @@ struct Dinic {
     }
     return level[t] != -1;
   }
-
   ll dfs(int u, ll flow) {
     if (!flow) return 0;
     if (u == t) return flow;
